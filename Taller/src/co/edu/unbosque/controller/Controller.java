73e10c76f750b223aff67c7ddbbf110e25b3c3a7
Familia <Familia@RodrAvilHP>
2025-02-23 15:18:02 -0500

    prueba

--- Taller/src/co/edu/unbosque/controller/Controller.java
+++ Taller/src/co/edu/unbosque/controller/Controller.java
@@ -1,5 +1,5 @@
 package co.edu.unbosque.controller;
-import java.util.Scanner;
+
 import co.edu.unbosque.view.ViewFacade;
 
 public class Controller {
@@ -8,6 +8,7 @@ public class Controller {
 
 	public Controller() {
 		vf = new ViewFacade();
+		run();
 	}
 
 	public void run() {
@@ -66,47 +67,101 @@ public class Controller {
 	}
 
 	public void solve10093() {
-		
-		        String[] inputs = {"3", "5", "A"}; // Sample inputs
+		ciclarProblem10093(false);
+	}
 
-		        for (String number : inputs) {
-		        	findSmallestBase(number);
-		            System.out.println();
-		        }
-		    
+	public void ciclarProblem10093(boolean continuar) {
+
+		continuar = true;
+		String in = vf.getCon().readLine();
+		if (in.equals("") || in == null) {
+			continuar = false;
 		}
 
-	public static String findSmallestBase(String number) {
-        String symbols = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-        int maxDigitValue = 2;
+		if (!continuar) {
+			return;
+		}
 
-        // Find the maximum digit value in the number
-        for (char c : number.toCharArray()) {
-            int digitValue = symbols.indexOf(c);
-            if (digitValue > maxDigitValue) {
-                maxDigitValue = digitValue;
-            }
-        }
+		ciclarComplemento10093(0, in);
 
-        // Minimum base must be greater than the highest digit
-        int minBase = maxDigitValue + 1 + 1;
+		ciclarProblem10093(continuar);
+	}
 
-        // Check for each base from minBase to 62
-        for (int base = minBase; base <= 62; base++) {
-            try {
-                long value = Long.parseLong(number, base);
-                if (value % (base - 1) == 0) {
-                    return String.valueOf(base);
-                }
-            } catch (NumberFormatException e) {
-                // Ignore invalid base conversions
-            }
-        }
+	public void ciclarComplemento10093(int i, String txt) {
 
-        return "such number is impossible!";
-    }	
-		
-	
+		int max = 0, sum = 0, n = 0;
+		char c = txt.charAt(i);
+
+		if (c >= '0' && c <= '9') {
+			n = c - '0';
+		} else if (c >= 'A' && c <= 'Z') {
+			n = c - 'A' + 10;
+		} else if (c >= 'a' && c <= 'z') {
+			n = c - 'a' + 36;
+		} else {
+			ciclarComplemento10093(i + 1, txt);
+		}
+
+		if (n > max)
+			max = n;
+		sum += n;
+		if (sum == 0) {
+			System.out.println("2");
+			ciclarComplemento10093(i + 1, txt);
+		}
+
+		ciclarComplemento210093(0, max, sum);
+
+	}
+
+	public void ciclarComplemento210093(int j, int max, int sum) {
+		boolean found = false;
+
+		int base = max + 1;
+		if (base >= 62) {
+			return;
+		}
+		if (sum % (base - 1) == 0) {
+			System.out.println(base);
+			found = true;
+			if (!found) {
+				System.out.println("such number is impossible!");
+			}
+			return;
+		}
+
+	}
+
+//	public void solutionM() {
+//		String[] inputs = { "3", "5", "A" }; 
+//
+//		for (String number : inputs) {
+//			System.out.println(findSmallestBase(number));
+//		}
+//	}
+//
+//	public static String findSmallestBase(String number) {
+//		String symbols = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+//		int maxDigitValue = 2;
+//
+//		for (char c : number.toCharArray()) {
+//			int digitValue = symbols.indexOf(c);
+//			if (digitValue > maxDigitValue) {
+//				maxDigitValue = digitValue;
+//			}
+//		}
+//
+//		int minBase = maxDigitValue + 1 + 1;
+//
+//		for (int base = minBase; base <= 62; base++) {
+//			long value = Long.parseLong(number, base);
+//			if (value % (base - 1) == 0) {
+//				return String.valueOf(base);
+//			}
+//		}
+//
+//		return "such number is impossible!";
+//	}
 
 	public void solve10579() {
 
