eedcc15f982003f50d9fb97d3f64b1c64c045fdd
Familia <Familia@RodrAvilHP>
2025-02-23 10:35:20 -0500

    1.2 cases

--- Taller/src/co/edu/unbosque/controller/Controller.java
+++ Taller/src/co/edu/unbosque/controller/Controller.java
@@ -11,12 +11,73 @@ public class Controller {
 	}
 
 	public void run() {
-		//a
 
-		String menu = """
+		mainloop: while (true) {
 
-				""";
-		System.out.println();
+			String menu = """
+
+					1. 623
+					2. 10093
+					3. 10579
+					4. 11068
+					5. 12696
+
+					6. Close
+
+					""";
+			System.out.println(menu);
+			int op = vf.getCon().readInt();
+			vf.getCon().burnLine();
+
+			switch (op) {
+			case 1:
+				solve623();
+				break;
+
+			case 2:
+				solve10093();
+				break;
+
+			case 3:
+				solve10579();
+				break;
+
+			case 4:
+				solve11068();
+				break;
+
+			case 5:
+				solve12696();
+				break;
+
+			case 6:
+				vf.getCon().printLine("BYE BYE");
+				break mainloop;
+
+			default:
+				vf.getCon().printLine("Invalid option");
+				break;
+			}
+		}
+	}
+
+	public void solve623() {
+
+	}
+
+	public void solve10093() {
+
+	}
+
+	public void solve10579() {
+
+	}
+
+	public void solve11068() {
+
+	}
+
+	public void solve12696() {
 
 	}
 
